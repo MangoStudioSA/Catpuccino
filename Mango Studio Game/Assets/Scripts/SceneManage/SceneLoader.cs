5f5ea06149de90293d984cf9d12487cf096a7c87
crisstinagl <crisgdl3011@gmail.com>
2025-10-01 19:50:03 +0200

    Create basic UI + Scenes
    Pantallas creadas: menu principal, opciones, creditos, juego, pausa y game over.
    Navegacion entre todas las pantallas mediante botones funcionales.

---
+++ Mango Studio Game/Assets/Scripts/SceneManage/SceneLoader.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Script encargado de realizar los cambios entre escenas
+public class SceneLoader : MonoBehaviour
+{
+    [Header("SceneLoader")]
+    public static SceneLoader Instance; // Instancia del SceneLoader en la escena
+    void Awake()
+    {
+        if (Instance == null) // Patron Singleton para mantener la informacion entre escenas
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Mantener entre escenas
+        }
+        else
+        {
+            Destroy(gameObject); // Si ya hay un SceneLoader en escena se destruyen los sobrantes
+        }
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu"); // Cargar escena menu principal
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game"); // Cargar escena juego
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("GameOver"); // Cargar escena fin del juego
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); // Salir del juego
+        Debug.Log("Saliendo del juego");
+    }
+
+}
