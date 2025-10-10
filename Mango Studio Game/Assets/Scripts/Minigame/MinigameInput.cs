621eb69fac07e6cf353cd299a035a51743114499
Gara03 <blancogarazi@gmail.com>
2025-10-10 15:58:32 +0200

    [v0.0.17]
    Mejora funcion de echar cafe

--- Mango Studio Game/Assets/Scripts/Minigame/MinigameInput.cs
+++ Mango Studio Game/Assets/Scripts/Minigame/MinigameInput.cs
@@ -6,11 +6,13 @@ public class MinigameInput : MonoBehaviour
     [SerializeField] GameObject coffeBar; //panel que contiene la barra
     [SerializeField] UnityEngine.UI.Slider coffeeSlider; //la barrita que se mueve
 
-    [SerializeField] float slideSpeed = 0.1f;
+    [SerializeField] float slideSpeed = 0.8f;
     [SerializeField] float maxAmount = 4.0f;
 
     float currentSlideTime = 0f;
     bool isSliding = false;
+
+    bool waitingForInput = false;
 
     PlayerOrder order;
 
@@ -24,71 +26,59 @@ public class MinigameInput : MonoBehaviour
             coffeeSlider.maxValue = maxAmount;
             coffeeSlider.value = 0f;
         }
-
-        if (coffeBar != null)
-        {
-            coffeBar.SetActive(false);
-        }
     }
 
     public void Update()
     {
+        //movimiento
         if (isSliding)
         {
-            currentSlideTime += Time.unscaledDeltaTime * slideSpeed;
+            currentSlideTime += Time.unscaledDeltaTime * slideSpeed; //no se por que pero solo me funciona si uso unscaled (no lo entiendo)
+
+            //el slider se actualiza con el tiempo de deslizamiento
+            coffeeSlider.value = currentSlideTime;
+
 
             if (currentSlideTime > maxAmount)
             {
                 currentSlideTime = maxAmount;
-                StopCoffeeSlide();
+                StopCoffee();
                 Debug.Log("La barrita llego al limite");
             }
+        }
+    }
 
-            coffeeSlider.value = currentSlideTime;
-        }
-    }
-    public void AddCoffe()
-    {
-        if (isSliding)
-        {
-            Debug.Log("Preparacion: Se ha detenido el cafe");
-            StopCoffeeSlide();
-        }
-        else
-        {
-            Debug.Log("Preparacion: Añadiendo cafe");
-            StartCoffeeSlide();
-        }
-    }
     
-    private void StartCoffeeSlide()
+    public void StartCoffee()
     {
         //reiniciamos la pos de la barra
         currentSlideTime = 0f;
-        coffeeSlider.value = 0f;
 
-        coffeBar.SetActive (true);
         isSliding = true;
+        Debug.Log("Preparacion: Carga de cafe iniciada.");
     }
     
-    private void StopCoffeeSlide()
+    public void StopCoffee()
     {
-        isSliding = false;
-        coffeBar.SetActive(false);
-
-        //guarda la pos del slider
-        if (order != null && order.currentOrder !=null)
+        if (isSliding)
         {
-            order.currentOrder.coffeePrecision = currentSlideTime;
+            // detenemos el movimiento
+            isSliding = false;
 
-            Debug.Log($"Slider de Café detenido en: {currentSlideTime:F2}. Cantidad: {order.currentOrder.coffeeAm}");
-        }
-        else
-        {
-            Debug.LogWarning($"Slider de Café detenido en: {currentSlideTime:F2}, pero no se pudo guardar porque no hay un pedido activo (order.currentOrder es null).");
+            // guarda la pos del slider
+            if (order != null && order.currentOrder != null)
+            {
+                order.currentOrder.coffeePrecision = currentSlideTime;
+                Debug.Log($"Preparacion: Cafe detenido en: {currentSlideTime:F2}. Valor guardado.");
+            }
+            else
+            {
+                Debug.LogWarning($"Preparacion: Cafe detenido en: {currentSlideTime:F2}, pero no se pudo guardar porque no hay un pedido activo.");
+            }
         }
 
     }
+
     
     public void Moler()
     {
