4d2ea8050ed316d44e4d242872391f55269dd533
crisstinagl <crisgdl3011@gmail.com>
2025-12-07 13:44:26 +0100

    [v2.0.30.2]
    Sonido ambiente + sonido lluvia implementados

--- Mango Studio Game/Assets/Scripts/Weather/WeatherController.cs
+++ Mango Studio Game/Assets/Scripts/Weather/WeatherController.cs
@@ -23,15 +23,23 @@ public class WeatherController : MonoBehaviour
 
     [Header("Clima soleado")]
     public float sunIntensitySunny = 1.5f;
-    public Color sunColorSunny = new Color(1f, 0.95f, 0.8f);
+    public Color sunColorSunny = new(1f, 0.95f, 0.8f);
     public float ambientIntensitySunny = 1.0f;
     [Range(0f, 1f)] public float sueloSeco = 0.2f;
 
     [Header("Clima lluvioso")]
     public float sunIntensityRainy = 0.2f;
-    public Color sunColorRainy = new Color(0.25f, 0.3f, 0.4f);
+    public Color sunColorRainy = new(0.25f, 0.3f, 0.4f);
     public float ambientIntensityRainy = 0.3f;
     [Range(0f, 1f)] public float sueloMojado = 0.9f;
+
+    [Header("Sonidos clima")]
+    public AudioClip rainClip;
+    public AudioClip ambientClip;
+    [Range(0f, 1f)] public float maxVolume = 0.5f;
+
+    private AudioSource rainSource;
+    private AudioSource ambientSource;
 
     private float normalIntensitySun; // Brillo normal del sol
     private float transitionVel = 0.5f; // Suavidad del cambio de luz
@@ -40,17 +48,41 @@ public class WeatherController : MonoBehaviour
     {
         if (timeManager == null) timeManager = FindFirstObjectByType<TimeManager>();
 
+        // Material del suelo
         if (rendererFloor != null)
         {
             materialFloor = rendererFloor.material;
             materialFloor.SetFloat("_Smoothness", sueloSeco);
         }
 
+        // Sonidos 
+        rainSource = CreateWeatherAudioSource("RainSource", rainClip);
+        ambientSource = CreateWeatherAudioSource("AmbientSource", ambientClip);
+
+        rainSource.volume = 0f;
+        rainSource.Play();
+
+        ambientSource.volume = maxVolume;
+        ambientSource.Play();
+
         // Se empieza sin lluvia
         if (rainSystem != null) rainSystem.Stop();
 
         // Se inicia el ciclo de clima
         StartCoroutine(WeatherCicle());
+    }
+
+    // Funcion para gestionar el sonido
+    AudioSource CreateWeatherAudioSource(string name, AudioClip clip)
+    {
+        GameObject audioObj = new GameObject(name);
+        audioObj.transform.parent = this.transform;
+        AudioSource source = audioObj.AddComponent<AudioSource>();
+        source.clip = clip;
+        source.loop = true;
+        source.playOnAwake = false;
+        source.spatialBlend = 0f;
+        return source;
     }
 
     // Funcion para calcular el tiempo a esperar
@@ -60,6 +92,7 @@ public class WeatherController : MonoBehaviour
         return gameHours * 60f * timeManager.secondsPerGameMinute;
     }
 
+    // Corrutina que gestiona los 2 ciclos del clima
     IEnumerator WeatherCicle()
     {
         // Empieza soleado
@@ -100,13 +133,21 @@ public class WeatherController : MonoBehaviour
         Color colorTarget = isRain ? sunColorRainy : sunColorSunny;
         float ambientTarget = isRain ? ambientIntensityRainy : ambientIntensitySunny;
 
+        float startIntensidad = sunLight.intensity;
+        Color startColor = sunLight.color;
+        float startAmbiente = RenderSettings.ambientIntensity;
+
+        // Material del suelo
         float targetSuelo = isRain ? sueloMojado : sueloSeco;
         float currentSuelo = materialFloor != null ? materialFloor.GetFloat("_Smoothness") : 0f;
         float timer = 0f;
 
-        float startIntensidad = sunLight.intensity;
-        Color startColor = sunLight.color;
-        float startAmbiente = RenderSettings.ambientIntensity;
+        // Sonidos 
+        float targetRainVol = isRain ? maxVolume : 0f;
+        float targetAmbientVol = isRain ? 0f : maxVolume;
+
+        float startRainVol = rainSource.volume;
+        float startAmbientVol = ambientSource.volume;
 
         while (timer < 1f)
         {
@@ -124,12 +165,22 @@ public class WeatherController : MonoBehaviour
                 materialFloor.SetFloat("_Smoothness", nuevoBrillo);
             }
 
+            if (rainSource != null)
+                rainSource.volume = Mathf.Lerp(startRainVol, targetRainVol, timer);
+
+            if (ambientSource != null)
+                ambientSource.volume = Mathf.Lerp(startAmbientVol, targetAmbientVol, timer);
+
             yield return null;
         }
 
         sunLight.intensity = intensityTarget;
         sunLight.color = colorTarget;
         RenderSettings.ambientIntensity = ambientTarget;
-        if (materialFloor != null) materialFloor.SetFloat("_Smoothness", targetSuelo);
+        if (materialFloor != null) materialFloor.SetFloat("_Smoothness", targetSuelo); // Material suelo
+
+        // Sonido
+        if (rainSource != null) rainSource.volume = targetRainVol;
+        if (ambientSource != null) ambientSource.volume = targetAmbientVol;
     }
 }
